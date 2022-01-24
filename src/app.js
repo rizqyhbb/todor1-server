945cc8ea692a4e25cdae09ce435f896814ef6872
rizqyhbb <rizqyhbb@gmail.com>
2022-01-25 05:32:22 +0700

    initiate express and sql

---
+++ src/app.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running on Port ${process.env.PORT}`)
+})
---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "port": 5432
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "port": 5432
+  },
+  "production": {
+    "use_env_variable": 'DATABASE_URL'
+  }
+}
